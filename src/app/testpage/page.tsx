227473f4b31114405a4c0fc3c196864a2721a038
Mattias <mattias.astrom@hotmail.com>
2025-10-16 11:25:17 +0200

    moved 3d components

--- src/app/testpage/page.tsx
+++ src/app/testpage/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 import Image from "next/image";
-import ClientSideSceneLoader from "../components/3d-components/ClientSideSceneLoader";
+import ClientSideSceneLoader from "../../components/3d-components/ClientSideSceneLoader";
 import {
   SceneContextProvider,
   useSceneContext,
-} from "../components/3d-components/SceneContext";
+} from "../../components/3d-components/SceneContext";
 import {
   AnimationProvider,
   useAnimation,
-} from "../components/3d-components/Animation/AnimationContext";
-import Chatbox from "../components/3d-components/tempChatbox ";
+} from "../../components/3d-components/Animation/AnimationContext";
+import Chatbox from "../../components/3d-components/tempChatbox ";
 
 //test page to wrap everything together and provide useContexts
 export default function TestPage() {
