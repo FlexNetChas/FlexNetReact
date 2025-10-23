bdc650dae29c226a0f4de4d9a8959014d6569588
JBrannelid <j.brannelid@icloud.com>
2025-10-23 16:59:00 +0200

    Added comments how getUserDescription behaves when fetching data from backend

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -3,8 +3,13 @@ import { requireAuth } from "@/lib/api/actions/authActions";
 import { getUserDescription } from "@/lib/api/actions/userDescriptionActions";
 import SettingsForm from "./SettingsForm";
 
+//Fetches user description data server-side before rendering the form and ensures user is authenticated
 async function SettingsPage() {
   const user = await requireAuth();
+
+  // Fetch user:s existing userDescription from backend
+  // If no user description exists, this will be null
+  // The form will handle this case by treating null value as a new user
   const userDescription = await getUserDescription(user.id);
 
   return <SettingsForm userId={user.id} initialData={userDescription} />;
