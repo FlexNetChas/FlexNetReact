9c0cb8583143af2808e6a1c08181f9bc2af3ab64
JBrannelid <j.brannelid@icloud.com>
2025-11-17 15:09:55 +0100

    Update responsive breakpoints for hero scetions

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,28 +9,31 @@ import { Meteors } from "./ui/shadcn-io/meteors";
 
 function Hero() {
   return (
-    <Section className="min-h-[calc(100vh-50px)] relative overflow-hidden">
+    <Section
+      spacing="sm"
+      className="min-h-[calc(100vh-50px)] relative overflow-hidden"
+    >
       <Meteors number={200} />
 
       <PageContainer className="flex items-center relative z-10">
-        <div className="grid w-full grid-cols-1 items-center gap-16 lg:grid-cols-2">
+        <div className="grid w-full grid-cols-1 items-center gap-0 md:gap-16 lg:grid-cols-2">
           <div className="space-y-8">
             <AnimatedText
               text="Welcome to Your Journey"
               gradientColors="linear-gradient(90deg, #3b82f6, #8b5cf6, #ec4899, #3b82f6)"
               gradientAnimationDuration={3}
               hoverEffect={true}
-              textClassName="!text-5xl md:!text-6xl !p-0 !m-0"
+              textClassName="!text-4xl sm:!text-5xl md:!text-6xl !p-0 !m-0"
             />
 
-            <h3 className="text-2xl sm:text-3xl lg:text-4xl">
+            <h3 className="text-xl sm:text-3xl lg:text-4xl">
               Find your path. Shape your your future
             </h3>
             <div className="flex flex-wrap gap-4">
               <Link href="/register">
                 <Button
                   variant="default"
-                  className="sm:px-6 sm:py-6 px-7 py-5 font-semibold "
+                  className="sm:px-6 sm:py-6 px-4 py-4 font-semibold "
                 >
                   Get Started Free
                 </Button>
